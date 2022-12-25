97ff4cffacd7e7b98f73dcf69594a71f64f7d5fb
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-25 22:46:17 +0300

    Initial Commit

---
+++ SciFiQuotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SciFiQuotes
+//
+//  Created by Николай Никитин on 25.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
